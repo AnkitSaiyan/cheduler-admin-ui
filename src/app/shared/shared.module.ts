2b2f9fbded70cc1facca81a258a4d56b6f7c2a70
Ankit Saiyan <ankit.kumar@remotestate.com>
2022-12-30 13:27:37 +0530

    post-it card done

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -2,6 +2,7 @@ import {NgModule} from '@angular/core';
 import {CommonModule} from '@angular/common';
 import {DesignSystemCoreModule, DesignSystemModule, TableModule} from "diflexmo-angular-design";
 import {NgChartsModule} from "ng2-charts";
+import {MdbCarouselModule} from "mdb-angular-ui-kit/carousel";
 
 @NgModule({
   declarations: [],
@@ -13,6 +14,7 @@ import {NgChartsModule} from "ng2-charts";
     DesignSystemCoreModule,
     TableModule,
     NgChartsModule,
+    MdbCarouselModule
   ]
 })
 export class SharedModule {
