c9b9c31affebd610e40ec9c94ee77006d4ece914
Ankit Saiyan <ankit.kumar@remotestate.com>
2023-01-31 20:19:57 +0530

    calendar month view created

---
+++ src/app/shared/components/dfm-calendar/dfm-calendar-week-view/dfm-calendar-week-view.component.ts
@@ -0,0 +1,12 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'dfm-calendar-week-view',
+  templateUrl: './dfm-calendar-week-view.component.html',
+  styleUrls: ['./dfm-calendar-week-view.component.scss'],
+})
+export class DfmCalendarWeekViewComponent implements OnInit {
+  constructor() {}
+
+  public ngOnInit(): void {}
+}
