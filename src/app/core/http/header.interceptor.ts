7e919330aae278924dc3c52e5f772f422e0dc9c0
Ankit Saiyan <ankit.kumar@remotestate.com>
2023-02-20 13:01:33 +0530

    interceptor updated

--- src/app/core/http/header.interceptor.ts
+++ src/app/core/http/header.interceptor.ts
@@ -1,22 +1,13 @@
 import { Injectable } from '@angular/core';
-import { HttpRequest, HttpHandler, HttpEvent, HttpInterceptor } from '@angular/common/http';
+import { HttpEvent, HttpHandler, HttpInterceptor, HttpRequest } from '@angular/common/http';
 import { Observable } from 'rxjs';
-import { environment } from '../../../environments/environment';
 
 @Injectable()
 export class HeaderInterceptor implements HttpInterceptor {
   constructor() {}
 
   public intercept(request: HttpRequest<unknown>, next: HttpHandler): Observable<HttpEvent<unknown>> {
-    let SubDomain: string;
-
-    console.log(window.location.host.split('.'));
-
-    if (environment.production) {
-      SubDomain = 'red-sea-08bb7b903';
-    } else {
-      SubDomain = 'localhost';
-    }
+    const SubDomain: string = window.location.host.split('.')[0];
 
     const newRequest = request.clone({
       setHeaders: {
