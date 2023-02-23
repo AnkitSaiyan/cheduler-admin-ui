9b40647075421f4c2fc7bfc6be58db050cf4e38d
ankit.kumar <ankit.kumar@mindrops.com>
2023-02-23 19:43:10 +0530

    feat: add title

--- src/app/core/core-routing.module.ts
+++ src/app/core/core-routing.module.ts
@@ -9,42 +9,52 @@ const routes: Routes = [
     children: [
       {
         path: 'dashboard',
+        title: 'Diflexmo - Dashboard',
         loadChildren: async () => (await import('../modules/dashboard/dashboard.module')).DashboardModule,
       },
       {
         path: 'staff',
+        title: 'Diflexmo - Staff',
         loadChildren: async () => (await import('../modules/staff/staff.module')).StaffModule,
       },
       {
         path: 'room',
+        title: 'Diflexmo - Room',
         loadChildren: async () => (await import('../modules/rooms/rooms.module')).RoomsModule,
       },
       {
         path: 'physician',
+        title: 'Diflexmo - Physician',
         loadChildren: async () => (await import('../modules/physician/physician.module')).PhysicianModule,
       },
       {
         path: 'site-management',
+        title: 'Diflexmo - Site Management',
         loadChildren: async () => (await import('../modules/site-management/site-management.module')).SiteManagementModule,
       },
       {
         path: 'practice-hours',
+        title: 'Diflexmo - Practice Hours',
         loadChildren: async () => (await import('../modules/practice-hours/practice-hours.module')).PracticeHoursModule,
       },
       {
         path: 'user',
+        title: 'Diflexmo - User',
         loadChildren: async () => (await import('../modules/user/user.module')).UserModule,
       },
       {
         path: 'exam',
+        title: 'Diflexmo - Exam',
         loadChildren: async () => (await import('../modules/exam/exam.module')).ExamModule,
       },
       {
         path: 'absence',
+        title: 'Diflexmo - Absence',
         loadChildren: async () => (await import('../modules/absence/absence.module')).AbsenceModule,
       },
       {
         path: 'appointment',
+        title: 'Diflexmo - Appointment',
         loadChildren: async () => (await import('../modules/appointments/appointments.module')).AppointmentsModule,
       },
       {
