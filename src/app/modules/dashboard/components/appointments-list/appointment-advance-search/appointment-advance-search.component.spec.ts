c82a53a2b39e6bd0883b4782b15571c9e64fcf30
shani1 <shani@nimapinfotech.com>
2023-03-14 17:31:38 +0530

    dashboard bug fixes

---
+++ src/app/modules/dashboard/components/appointments-list/appointment-advance-search/appointment-advance-search.component.spec.ts
@@ -0,0 +1,23 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { AppointmentAdvanceSearchComponent } from './appointment-advance-search.component';
+
+describe('AppointmentAdvanceSearchComponent', () => {
+  let component: AppointmentAdvanceSearchComponent;
+  let fixture: ComponentFixture<AppointmentAdvanceSearchComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ AppointmentAdvanceSearchComponent ]
+    })
+    .compileComponents();
+
+    fixture = TestBed.createComponent(AppointmentAdvanceSearchComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
