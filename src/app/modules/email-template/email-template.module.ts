dd1dfb21a8854a8bd74c7e08467f17d11f68d421
AnkitSaiyan <ankit.kumar@mindrops.com>
2023-03-17 13:26:46 +0530

    Email template - code uncommented

--- src/app/modules/email-template/email-template.module.ts
+++ src/app/modules/email-template/email-template.module.ts
@@ -2,7 +2,7 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { EmailTemplateComponent } from './pages/email/email-template.component';
 import { EmailTemplateListComponent } from './components/email-template-list/email-template-list.component';
-import { EditEmailTemplateComponent } from './components/edit-email/edit-email-template.component';
+import { EditEmailTemplateComponent } from './components/edit-email-template/edit-email-template.component';
 import { EmailRoutingModule } from './email-routing.module';
 import { SharedModule } from 'src/app/shared/shared.module';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
