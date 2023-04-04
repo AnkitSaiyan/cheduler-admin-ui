23aef1d1991c9f7d8d3daf460607b56953ddcb95
ankit.kumar <ankit.kumar@mindrops.com>
2023-04-04 21:49:47 +0530

    add permission guard

--- src/app/core/guard/permission.guard.ts
+++ src/app/core/guard/permission.guard.ts
@@ -1,17 +1,65 @@
 import { Injectable } from '@angular/core';
-import { ActivatedRouteSnapshot, CanActivateChild, RouterStateSnapshot, UrlTree } from '@angular/router';
+import { ActivatedRouteSnapshot, CanActivateChild, Router, UrlTree } from '@angular/router';
 import { Observable } from 'rxjs';
+import { RouteName, RouteType } from 'src/app/shared/models/permission.model';
+import { UserRoleEnum } from 'src/app/shared/models/user.model';
+import { PermissionService } from '../services/permission.service';
 
 @Injectable({
   providedIn: 'root',
 })
 export class PermissionGuard implements CanActivateChild {
-  canActivateChild(
-    route: ActivatedRouteSnapshot,
-    state: RouterStateSnapshot,
-  ): Observable<boolean | UrlTree> | Promise<boolean | UrlTree> | boolean | UrlTree {
-    console.log(route, state, 'guard');
+  constructor(private permissionSvc: PermissionService, private router: Router) {}
+
+  canActivateChild(route: ActivatedRouteSnapshot): Observable<boolean | UrlTree> | Promise<boolean | UrlTree> | boolean | UrlTree {
+    switch (this.permissionSvc.permissionType$$.value) {
+      case UserRoleEnum.Reader: {
+        const isPermitted = this.isReaderPermitted(route);
+        return isPermitted;
+      }
+      default:
+        break;
+    }
+    return true;
+  }
+
+  private isReaderPermitted(route: ActivatedRouteSnapshot): boolean {
+    if (
+      route.data['routeType'] === RouteType.Add ||
+      route.data['routeName'] === RouteName.Practice ||
+      route.data['routeName'] === RouteName.SiteSetting
+    )
+      return false;
+    return true;
+  }
+
+  private isGeneralUserPermitted(route: ActivatedRouteSnapshot): boolean {
+    if (route.data['routeName'] === RouteName.Practice || route.data['routeName'] === RouteName.SiteSetting) return false;
     return true;
   }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
