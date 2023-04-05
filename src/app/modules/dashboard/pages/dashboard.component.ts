3224026ba5ec4b95700de7ef0cf37104c2c94fe7
AnkitSaiyan <ankit.kumar@mindrops.com>
2023-04-05 22:35:18 +0530

    Permission - dropdown value stay selected

--- src/app/modules/dashboard/pages/dashboard.component.ts
+++ src/app/modules/dashboard/pages/dashboard.component.ts
@@ -1,8 +1,10 @@
 import { Component, OnInit } from '@angular/core';
 import { FormControl } from '@angular/forms';
-import { filter } from 'rxjs';
+import {filter, take, takeUntil} from 'rxjs';
 import { PermissionService } from 'src/app/core/services/permission.service';
 import { UserRoleEnum } from 'src/app/shared/models/user.model';
+import {NameValue} from "../../../shared/components/search-modal.component";
+import {UserApiService} from "../../../core/services/user-api.service";
 
 @Component({
   selector: 'dfm-dashboard',
@@ -10,28 +12,23 @@ import { UserRoleEnum } from 'src/app/shared/models/user.model';
   styleUrls: ['./dashboard.component.scss'],
 })
 export class DashboardComponent implements OnInit {
-  public userRole: { name: any; value: any }[] = [
-    {
-      name: UserRoleEnum.Admin,
-      value: UserRoleEnum.Admin,
-    },
-    {
-      name: UserRoleEnum.GeneralUser,
-      value: UserRoleEnum.GeneralUser,
-    },
-    {
-      name: UserRoleEnum.Reader,
-      value: UserRoleEnum.Reader,
-    },
-  ];
+  public userRoles: NameValue[] = [];
 
-  public userRoleControl = new FormControl(UserRoleEnum.Admin, []);
+  public userRoleControl = new FormControl();
 
-  constructor(private permissionSvc: PermissionService) {}
+  constructor(private userSvc: UserApiService, private permissionSvc: PermissionService) {}
 
   ngOnInit(): void {
+    this.userRoles = [...this.userSvc.getUserRoles()];
+
+    this.permissionSvc.permissionType$.pipe(take(1)).subscribe((role) => {
+      console.log('role', role);
+      setTimeout(() => {
+        this.userRoleControl.setValue(role);
+      }, 0);
+    });
+
     this.userRoleControl.valueChanges.pipe(filter(Boolean)).subscribe((value) => {
-      localStorage.setItem('userRole', value);
       this.permissionSvc.setPermissionType(value);
     });
   }
