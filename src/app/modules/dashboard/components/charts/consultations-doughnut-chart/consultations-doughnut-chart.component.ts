ab0a3cd31f92f5e393d17117fba9b94d6a0b9099
ankit.kumar <ankit.kumar@mindrops.com>
2023-04-07 16:18:57 +0530

    dashboard - add complete and cancelled growth api

--- src/app/modules/dashboard/components/charts/consultations-doughnut-chart/consultations-doughnut-chart.component.ts
+++ src/app/modules/dashboard/components/charts/consultations-doughnut-chart/consultations-doughnut-chart.component.ts
@@ -6,98 +6,101 @@ import { DestroyableComponent } from 'src/app/shared/components/destroyable.comp
 import { BehaviorSubject, takeUntil } from 'rxjs';
 
 @Component({
-  selector: 'dfm-consultations-doughnut-chart',
-  templateUrl: './consultations-doughnut-chart.component.html',
-  styleUrls: ['./consultations-doughnut-chart.component.scss'],
+	selector: 'dfm-consultations-doughnut-chart',
+	templateUrl: './consultations-doughnut-chart.component.html',
+	styleUrls: ['./consultations-doughnut-chart.component.scss'],
 })
 export class ConsultationsDoughnutChartComponent extends DestroyableComponent implements OnInit {
-  public completedBarChartLabels = ['Su', 'Mo', 'Tu', 'We', 'Th', 'Fr', 'Sa'];
+	public completedBarChartLabels = ['Su', 'Mo', 'Tu', 'We', 'Th', 'Fr', 'Sa'];
 
-  public cancelledBarChartOptions!: ChartOptions<'bar'>;
+	public cancelledBarChartOptions!: ChartOptions<'bar'>;
 
-  public cancelledBarChartConfig!: ChartConfiguration<'bar'>['data'];
+	public cancelledBarChartConfig!: ChartConfiguration<'bar'>['data'];
 
-  public cancelledBarChartLegend = false;
+	public cancelledBarChartLegend = false;
 
-  public cancelledBarChartPlugins = [pluginDataLabels.default];
+	public cancelledBarChartPlugins = [pluginDataLabels.default];
 
-  constructor(private dashboardApiService: DashboardApiService) {
-    super();
-  }
+	public status$$ = new BehaviorSubject<any>(null);
 
-  public ngOnInit(): void {
-    this.dashboardApiService.completedBarChart$.pipe(takeUntil(this.destroy$$)).subscribe((appointment) => {
-      const dataset: any = Array(7).fill(0);
-      appointment.completedappointments.forEach((element) => {
-        switch (true) {
-          case element?.label === 'Sunday':
-            dataset[0] = element.value;
-            break;
-          case element?.label === 'Monday':
-            dataset[1] = element.value;
-            break;
-          case element?.label === 'Tuesday':
-            dataset[2] = element.value;
-            break;
-          case element?.label === 'Wednesday':
-            dataset[3] = element.value;
-            break;
-          case element?.label === 'Thursday':
-            dataset[4] = element.value;
-            break;
-          case element?.label === 'Friday':
-            dataset[5] = element.value;
-            break;
-          case element?.label === 'Saturday':
-            dataset[6] = element.value;
-            break;
-          default:
-            break;
-        }
-      });
+	constructor(private dashboardApiService: DashboardApiService) {
+		super();
+	}
 
-      this.cancelledBarChartConfig = {
-        labels: this.completedBarChartLabels,
-        datasets: [
-          {
-            barPercentage: 0.5,
-            // categoryPercentage: 1.0,
-            data: dataset,
-            backgroundColor: ['#F7B27A'],
-            hoverBackgroundColor: ['#EF6820'],
-          },
-        ],
-      };
+	public ngOnInit(): void {
+		this.dashboardApiService.completedBarChart$.pipe(takeUntil(this.destroy$$)).subscribe((appointment) => {
+			this.status$$.next(appointment?.status);
+			const dataset: any = Array(7).fill(0);
+			appointment.completedappointments.forEach((element) => {
+				switch (true) {
+					case element?.label === 'Sunday':
+						dataset[0] = element.value;
+						break;
+					case element?.label === 'Monday':
+						dataset[1] = element.value;
+						break;
+					case element?.label === 'Tuesday':
+						dataset[2] = element.value;
+						break;
+					case element?.label === 'Wednesday':
+						dataset[3] = element.value;
+						break;
+					case element?.label === 'Thursday':
+						dataset[4] = element.value;
+						break;
+					case element?.label === 'Friday':
+						dataset[5] = element.value;
+						break;
+					case element?.label === 'Saturday':
+						dataset[6] = element.value;
+						break;
+					default:
+						break;
+				}
+			});
 
-      this.cancelledBarChartOptions = {
-        responsive: true,
-        scales: {
-          x: {
-            display: true,
-          },
-          y: {
-            display: false,
-          },
-        },
-        plugins: {
-          legend: {
-            display: false,
-          },
-          datalabels: {
-            anchor: 'end',
-            align: 'end',
-            color: '#EF6820',
-            font: {
-              size: 8,
-            },
-          },
-        },
-      };
-    });
-  }
+			this.cancelledBarChartConfig = {
+				labels: this.completedBarChartLabels,
+				datasets: [
+					{
+						barPercentage: 0.5,
+						// categoryPercentage: 1.0,
+						data: dataset,
+						backgroundColor: ['#F7B27A'],
+						hoverBackgroundColor: ['#EF6820'],
+					},
+				],
+			};
 
-  // this.doughnutChartDatasets = [{
-  //   data: {
-  //   }
-  // }]
+			this.cancelledBarChartOptions = {
+				responsive: true,
+				scales: {
+					x: {
+						display: true,
+					},
+					y: {
+						display: false,
+					},
+				},
+				plugins: {
+					legend: {
+						display: false,
+					},
+					datalabels: {
+						anchor: 'end',
+						align: 'end',
+						color: '#EF6820',
+						font: {
+							size: 8,
+						},
+					},
+				},
+			};
+		});
+	}
+
+	// this.doughnutChartDatasets = [{
+	//   data: {
+	//   }
+	// }]
 }
