b298d39796fd5c30d6c23c2300a68f76d65f2425
AnkitSaiyan <ankit.kumar@mindrops.com>
2023-04-10 12:05:48 +0530

    User role dropdown added

--- src/app/modules/dashboard/pages/dashboard.component.ts
+++ src/app/modules/dashboard/pages/dashboard.component.ts
@@ -1,11 +1,37 @@
-import {Component} from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {UserApiService} from "../../../core/services/user-api.service";
+import {NameValue} from "../../../shared/components/search-modal.component";
+import {FormControl} from "@angular/forms";
+import {debounceTime, take} from "rxjs";
+import {PermissionService} from "../../../core/services/permission.service";
 
 @Component({
     selector: 'dfm-dashboard',
     templateUrl: './dashboard.component.html',
     styleUrls: ['./dashboard.component.scss'],
 })
-export class DashboardComponent {
+export class DashboardComponent implements OnInit {
+    public userRoleControl: FormControl = new FormControl('', []);
+    public userRoles: NameValue[] = [];
+
+    public constructor(
+        private userApiSvc: UserApiService,
+        private permissionSvc: PermissionService
+    ) {
+    }
+
+    public ngOnInit() {
+        this.userRoles = this.userApiSvc.getRoleTypes();
+
+        this.userRoleControl.valueChanges.pipe(debounceTime(0)).subscribe((userRole) => {
+            this.permissionSvc.setPermissionType(userRole);
+        });
+
+        this.permissionSvc.permissionType$.pipe(take(1)).subscribe((userRole) => {
+            setTimeout(() => this.userRoleControl.setValue(userRole), 0);
+        });
+    }
+
     public handleClick() {
         document.querySelector('#top')?.scrollIntoView({block: 'start', behavior: 'smooth'});
     }
