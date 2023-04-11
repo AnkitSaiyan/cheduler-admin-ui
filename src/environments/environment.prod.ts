c1789fa3401f2d40f638f7eb3791c16a548b3318
AnkitSaiyan <ankit.kumar@mindrops.com>
2023-04-11 22:34:56 +0530

    redirect uri bug fix try 1

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -12,6 +12,6 @@ export const environment: Environment = {
   schedulerApiUrl: 'https://diflexmo-scheduler-api-dev.azurewebsites.net/api',
   userManagementApiUrl: 'https://auth.diflexmo.be/usermanagement/api/',
   authClientId: 'd526e147-4713-4a0a-bf56-d8f500fb9a62',
-  redirectUrl: 'https://red-sea-08bb7b903.2.azurestaticapps.net',
+  redirectUrl: 'https://red-sea-08bb7b903.2.azurestaticapps.net/',
   schedulerApiAuthScope: 'https://diflexmoauth.onmicrosoft.com/cheduler.api/cheduler.api',
 };
