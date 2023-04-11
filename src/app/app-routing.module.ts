2627406b44b366d4fd14e2923dec069ba7c924db
ankit.kumar <ankit.kumar@mindrops.com>
2023-04-11 17:09:57 +0530

    admin auth - fix: complete profile route guard

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,37 +1,36 @@
-import {NgModule} from '@angular/core';
-import {RouterModule, Routes} from '@angular/router';
-import {BrowserUtils} from '@azure/msal-browser';
-import {MsalGuard} from '@azure/msal-angular';
-import {CompleteProfileComponent} from "./shared/components/complete-profile/complete-profile.component";
-import {RouteGuard} from "./core/guard/route.guard";
-import {LoginFailedComponent} from "./shared/components/login-failed/login-failed.component";
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { BrowserUtils } from '@azure/msal-browser';
+import { MsalGuard } from '@azure/msal-angular';
+import { CompleteProfileComponent } from './shared/components/complete-profile/complete-profile.component';
+import { RouteGuard } from './core/guard/route.guard';
+import { LoginFailedComponent } from './shared/components/login-failed/login-failed.component';
 
 const rootRoutes: Routes = [
-    {
-        path: 'complete-profile',
-        title: 'Cheduler - Complete Profile',
-        component: CompleteProfileComponent,
-        canActivate: [RouteGuard, MsalGuard],
-    },
-    {
-        path: '',
-        loadChildren: async () => (await import('./core/core.module')).CoreModule,
-        canActivate: [RouteGuard, MsalGuard],
-    },
-    {
-        path: '',
-        title: 'Login Failed',
-        component: LoginFailedComponent
-    }
+	{
+		path: 'complete-profile',
+		title: 'Cheduler - Complete Profile',
+		component: CompleteProfileComponent,
+		canActivate: [MsalGuard, RouteGuard],
+	},
+	{
+		path: '',
+		loadChildren: async () => (await import('./core/core.module')).CoreModule,
+		canActivate: [MsalGuard, RouteGuard],
+	},
+	{
+		path: '',
+		title: 'Login Failed',
+		component: LoginFailedComponent,
+	},
 ];
 
 @NgModule({
-    imports: [
-        RouterModule.forRoot(rootRoutes, {
-            initialNavigation: !BrowserUtils.isInIframe() && !BrowserUtils.isInPopup() ? 'enabledNonBlocking' : 'disabled', // Set to enabledBlocking to use Angular Universal
-        }),
-    ],
-    exports: [RouterModule],
+	imports: [
+		RouterModule.forRoot(rootRoutes, {
+			initialNavigation: !BrowserUtils.isInIframe() && !BrowserUtils.isInPopup() ? 'enabledNonBlocking' : 'disabled', // Set to enabledBlocking to use Angular Universal
+		}),
+	],
+	exports: [RouterModule],
 })
-export class AppRoutingModule {
-}
+export class AppRoutingModule {}
--- src/app/core/guard/route.guard.ts
+++ src/app/core/guard/route.guard.ts
@@ -1,72 +1,69 @@
-import {Injectable} from "@angular/core";
-import {
-    ActivatedRouteSnapshot,
-    CanActivate,
-    CanActivateChild,
-    Router,
-    RouterStateSnapshot,
-    UrlTree
-} from "@angular/router";
-import {debounceTime, map, Observable} from "rxjs";
-import {RouteName} from "../../shared/models/permission.model";
-import {AuthUser} from "../../shared/models/user.model";
-import {UserService} from "../services/user.service";
+import { Injectable } from '@angular/core';
+import { ActivatedRouteSnapshot, CanActivate, CanActivateChild, Router, RouterStateSnapshot, UrlTree } from '@angular/router';
+import { debounceTime, filter, map, Observable } from 'rxjs';
+import { RouteName } from '../../shared/models/permission.model';
+import { AuthUser } from '../../shared/models/user.model';
+import { UserService } from '../services/user.service';
 
 @Injectable({
-    providedIn: 'root'
+	providedIn: 'root',
 })
 export class RouteGuard implements CanActivate, CanActivateChild {
-    constructor(private userService: UserService, private router: Router) {
-    }
+	constructor(private userService: UserService, private router: Router) {}
 
-    public canActivate(route: ActivatedRouteSnapshot, state: RouterStateSnapshot): Observable<boolean | UrlTree> {
-        const {url} = state;
-        return this.isProfileIncomplete(url);
-    }
+	public canActivate(route: ActivatedRouteSnapshot, state: RouterStateSnapshot): Observable<boolean | UrlTree> {
+		const { url } = state;
+		return this.isProfileIncomplete(url);
+	}
 
-    public canActivateChild(childRoute: ActivatedRouteSnapshot, state: RouterStateSnapshot): Observable<boolean | UrlTree> | Promise<boolean | UrlTree> | boolean | UrlTree {
-        const {url} = state;
-        return this.isProfileIncomplete(url);
-    }
+	public canActivateChild(
+		childRoute: ActivatedRouteSnapshot,
+		state: RouterStateSnapshot,
+	): Observable<boolean | UrlTree> | Promise<boolean | UrlTree> | boolean | UrlTree {
+		const { url } = state;
+		return this.isProfileIncomplete(url);
+	}
 
-    private isProfileIncomplete(url: string) {
-        return this.userService.authUser$.pipe(
-            debounceTime(0),
-            map((user) => {
-                try {
-                    const isIncomplete = !!user?.properties['extension_ProfileIsIncomplete'];
+	private isProfileIncomplete(url: string) {
+		return this.userService.authUser$.pipe(
+			debounceTime(0),
+			filter(Boolean),
+			map((user) => {
+				try {
+					const isIncomplete = !!user?.properties['extension_ProfileIsIncomplete'];
 
-                    console.log('Profile is incomplete', isIncomplete);
+					console.log('Profile is incomplete', isIncomplete);
 
-                    const splitUrl = url.split('/')[1];
+					const splitUrl = url.split('/')[1];
 
-                    console.log('URL Name', splitUrl);
+					console.log('URL Name', splitUrl);
 
-                    switch (splitUrl) {
-                        case RouteName.LoginFailed: {
-                            if (AuthUser) {
-                                return this.router.parseUrl('/');
-                            }
-                            break;
-                        }
-                        case RouteName.CompleteProfile: {
-                            if (!isIncomplete) {
-                                return this.router.parseUrl(`/`);
-                            }
-                            break;
-                        }
-                        default: {
-                            if (isIncomplete) {
-                                return this.router.parseUrl(`/${RouteName.CompleteProfile}`);
-                            }
-                        }
-                    }
+					switch (splitUrl) {
+						case RouteName.LoginFailed: {
+							if (AuthUser) {
+								return this.router.parseUrl('/');
+							}
+							break;
+						}
+						case RouteName.CompleteProfile: {
+							if (!isIncomplete) {
+								return this.router.parseUrl(`/`);
+							}
+							break;
+						}
+						default: {
+							if (isIncomplete) {
+								return this.router.parseUrl(`/${RouteName.CompleteProfile}`);
+							}
+						}
+					}
 
-                    return true;
-                } catch (e) {
-                    return false;
-                }
-            })
-        )
-    }
+					return true;
+				} catch (e) {
+					return false;
+				}
+			}),
+		);
+	}
 }
+
