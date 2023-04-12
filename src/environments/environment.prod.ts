14bf9badf35b9f047bbcebc1a04ab0988065a640
AnkitSaiyan <ankit.kumar@mindrops.com>
2023-04-12 12:13:50 +0530

    redirect url is now taken dynamically

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -12,6 +12,6 @@ export const environment: Environment = {
   schedulerApiUrl: 'https://diflexmo-scheduler-api-dev.azurewebsites.net/api',
   userManagementApiUrl: 'https://auth.diflexmo.be/usermanagement/api/',
   authClientId: 'd526e147-4713-4a0a-bf56-d8f500fb9a62',
-  redirectUrl: window.location.host,
+  redirectUrl: window.location.origin,
   schedulerApiAuthScope: 'https://diflexmoauth.onmicrosoft.com/cheduler.api/cheduler.api',
 };
