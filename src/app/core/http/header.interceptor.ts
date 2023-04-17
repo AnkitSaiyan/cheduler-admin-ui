3389962f6310ccee2bcbfa23a0fb245038a0135b
AnkitSaiyan <ankit.kumar@mindrops.com>
2023-04-17 11:09:24 +0530

    Sending tenant id in user management /users api

--- src/app/core/http/header.interceptor.ts
+++ src/app/core/http/header.interceptor.ts
@@ -1,22 +1,27 @@
-import { Injectable } from '@angular/core';
-import { HttpEvent, HttpHandler, HttpInterceptor, HttpRequest } from '@angular/common/http';
-import { Observable } from 'rxjs';
+import {Injectable} from '@angular/core';
+import {HttpEvent, HttpHandler, HttpInterceptor, HttpRequest} from '@angular/common/http';
+import {Observable} from 'rxjs';
+import {environment} from "../../../environments/environment";
+import {UserService} from "../services/user.service";
 
 @Injectable()
 export class HeaderInterceptor implements HttpInterceptor {
-  constructor() {}
+    constructor(private userSvc: UserService) {
+    }
 
-  public intercept(request: HttpRequest<unknown>, next: HttpHandler): Observable<HttpEvent<unknown>> {
-    // console.log(window.location);
+    public intercept(request: HttpRequest<unknown>, next: HttpHandler): Observable<HttpEvent<unknown>> {
+        const SubDomain: string = window.location.host.split('.')[0];
+        const usersUrl = environment.userManagementApiUrl + '/users';
 
-    const SubDomain: string = window.location.host.split('.')[0];
+        const newRequest = request.clone({
+            setHeaders: {
+                SubDomain,
+                ...(usersUrl === request.url ? {
+                    'Context-Tenant-Id': this.userSvc.getCurrentUser()?.tenantIds[0] as string
+                } : {})
+            },
+        });
 
-    const newRequest = request.clone({
-      setHeaders: {
-        SubDomain,
-      },
-    });
-
-    return next.handle(newRequest);
-  }
+        return next.handle(newRequest);
+    }
 }
