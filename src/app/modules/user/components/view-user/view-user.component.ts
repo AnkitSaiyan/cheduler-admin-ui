6449914fc5137421db0cfdc633c80b7e5cec8a8f
ankit.kumar <ankit.kumar@mindrops.com>
2023-05-10 14:41:10 +0530

    admin fix: general user edit form detail view

--- src/app/modules/user/components/view-user/view-user.component.ts
+++ src/app/modules/user/components/view-user/view-user.component.ts
@@ -1,24 +1,21 @@
 import {Component, OnDestroy, OnInit} from '@angular/core';
-import {BehaviorSubject, combineLatest, filter, map, Observable, switchMap, take, takeUntil} from 'rxjs';
-import {ActivatedRoute, Router} from '@angular/router';
-import {DestroyableComponent} from '../../../../shared/components/destroyable.component';
-import {RouterStateService} from '../../../../core/services/router-state.service';
-import {NotificationDataService} from '../../../../core/services/notification-data.service';
-import {ModalService} from '../../../../core/services/modal.service';
-import {ENG_BE, STAFF_ID} from '../../../../shared/utils/const';
-import {
-    ConfirmActionModalComponent,
-    ConfirmActionModalData
-} from '../../../../shared/components/confirm-action-modal.component';
-import {AddUserComponent} from '../add-user/add-user.component';
-import {User, UserType} from '../../../../shared/models/user.model';
-import {getUserTypeEnum} from '../../../../shared/utils/getEnums';
-import {Translate} from '../../../../shared/models/translate.model';
-import {ShareDataService} from 'src/app/core/services/share-data.service';
-import {UserManagementApiService} from "../../../../core/services/user-management-api.service";
-import {Permission} from "../../../../shared/models/permission.model";
-import {UserApiService} from "../../../../core/services/user-api.service";
-import {UserService} from "../../../../core/services/user.service";
+import { BehaviorSubject, combineLatest, filter, map, Observable, switchMap, take, takeUntil, tap } from 'rxjs';
+import { ActivatedRoute, Router } from '@angular/router';
+import { DestroyableComponent } from '../../../../shared/components/destroyable.component';
+import { RouterStateService } from '../../../../core/services/router-state.service';
+import { NotificationDataService } from '../../../../core/services/notification-data.service';
+import { ModalService } from '../../../../core/services/modal.service';
+import { ENG_BE, STAFF_ID } from '../../../../shared/utils/const';
+import { ConfirmActionModalComponent, ConfirmActionModalData } from '../../../../shared/components/confirm-action-modal.component';
+import { AddUserComponent } from '../add-user/add-user.component';
+import { User, UserType } from '../../../../shared/models/user.model';
+import { getUserTypeEnum } from '../../../../shared/utils/getEnums';
+import { Translate } from '../../../../shared/models/translate.model';
+import { ShareDataService } from 'src/app/core/services/share-data.service';
+import { UserManagementApiService } from '../../../../core/services/user-management-api.service';
+import { Permission } from '../../../../shared/models/permission.model';
+import { UserApiService } from '../../../../core/services/user-api.service';
+import { UserService } from '../../../../core/services/user.service';
 
 @Component({
 	selector: 'dfm-view-user',
@@ -71,7 +68,10 @@ export class ViewUserComponent extends DestroyableComponent implements OnInit, O
 							),
 						);
 					}
-					return this.userApiSvc.getUserByID$(+userID) as Observable<User>;
+					return this.userApiSvc.getUserByID$(+userID).pipe() as Observable<User>;
+				}),
+				tap((user) => {
+					this.currentUserType = user.userType;
 				}),
 			),
 		])
