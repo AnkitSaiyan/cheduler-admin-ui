2fb04c1c43ca89bd0535f6a7681328a5daa14e16
ankit.kumar <ankit.kumar@mindrops.com>
2023-05-17 13:51:29 +0530

    admin - appointment day view ngbMenu replaced with mat menu

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,9 +1,14 @@
-import { ModuleWithProviders, NgModule } from '@angular/core';
+import { ClipboardModule } from '@angular/cdk/clipboard';
+import { DragDropModule } from '@angular/cdk/drag-drop';
 import { CommonModule, NgOptimizedImage } from '@angular/common';
-import { DesignSystemModule, NgDfmDropdownModule, TableModule } from 'diflexmo-angular-design';
-import { MatButtonModule } from '@angular/material/button';
-import { MdbCarouselModule } from 'mdb-angular-ui-kit/carousel';
+import { HttpClient } from '@angular/common/http';
+import { ModuleWithProviders, NgModule } from '@angular/core';
 import { ReactiveFormsModule } from '@angular/forms';
+import { MatButtonModule } from '@angular/material/button';
+import { MatDividerModule } from '@angular/material/divider';
+import { MatIconModule } from '@angular/material/icon';
+import { MatProgressSpinner, MatProgressSpinnerModule } from '@angular/material/progress-spinner';
+import { RouterLink } from '@angular/router';
 import {
 	NgbAccordionModule,
 	NgbDatepickerModule,
@@ -12,62 +17,58 @@ import {
 	NgbPopoverModule,
 	NgbTimepickerModule,
 } from '@ng-bootstrap/ng-bootstrap';
-import { RouterLink } from '@angular/router';
+import { DesignSystemModule, NgDfmDropdownModule, TableModule } from 'diflexmo-angular-design';
+import { MdbCarouselModule } from 'mdb-angular-ui-kit/carousel';
 import { NgChartsModule } from 'ng2-charts';
-import { DragDropModule } from '@angular/cdk/drag-drop';
-import { ClipboardModule } from '@angular/cdk/clipboard';
-import { HttpClient } from '@angular/common/http';
-import { MatProgressSpinner, MatProgressSpinnerModule } from '@angular/material/progress-spinner';
-import { MatDividerModule } from '@angular/material/divider';
-import { MatIconModule } from '@angular/material/icon';
-import { DestroyableComponent } from './components/destroyable.component';
-import { StatusNamePipe } from './pipes/status-name.pipe';
-import { DashIfNothingPipe } from './pipes/dash-if-nothing.pipe';
-import { ConfirmStatusChangeBannerComponent } from './components/confirm-status-change-banner.component';
-import { WeekdayToNamePipe } from './pipes/weekday-to-name.pipe';
-import { InputDropdownWrapperComponent } from './components/input-dropdown-wrapper.component';
 import { ConfirmActionModalComponent } from './components/confirm-action-modal.component';
-import { TimeInIntervalPipe } from './pipes/time-in-interval.pipe';
-import { SearchModalComponent } from './components/search-modal.component';
-import { DfmSpinnerComponent } from './components/dfm-spinner.component';
-import { MonthToNamePipe } from './pipes/month-to-name.pipe';
-import { JoinWithAndPipe } from './pipes/join-with-and.pipe';
-import { NameValuePairPipe } from './pipes/name-value-pair.pipe';
-import { ApprovalTypeNamePipe } from './pipes/approval-type-name.pipe';
-import { SumPipe } from './pipes/sum.pipe';
-import { RepeatTypeToNamePipe } from './pipes/repeat-type-to-name.pipe';
-import { AppendTextToInputDirective } from './directives/append-text-to-input.directive';
+import { ConfirmStatusChangeBannerComponent } from './components/confirm-status-change-banner.component';
+import { DestroyableComponent } from './components/destroyable.component';
+import { DfmCalendarDayViewComponent } from './components/dfm-calendar/dfm-calendar-day-view/dfm-calendar-day-view.component';
 import { DfmCalendarMonthViewComponent } from './components/dfm-calendar/dfm-calendar-month-view/dfm-calendar-month-view.component';
 import { DfmCalendarWeekViewComponent } from './components/dfm-calendar/dfm-calendar-week-view/dfm-calendar-week-view.component';
-import { DfmCalendarDayViewComponent } from './components/dfm-calendar/dfm-calendar-day-view/dfm-calendar-day-view.component';
-import { NumberToDatePipe } from './pipes/number-to-date.pipe';
-import { LargestPipe } from './pipes/largest.pipe';
-import { NameInputDirective } from './directives/name-input.directive';
+import { DfmSpinnerComponent } from './components/dfm-spinner.component';
+import { InputDropdownWrapperComponent } from './components/input-dropdown-wrapper.component';
+import { SearchModalComponent } from './components/search-modal.component';
+import { AppendTextToInputDirective } from './directives/append-text-to-input.directive';
 import { EmailInputDirective } from './directives/email-input.directive';
+import { NameInputDirective } from './directives/name-input.directive';
 import { NumberInputDirective } from './directives/number-input.directive';
+import { ApprovalTypeNamePipe } from './pipes/approval-type-name.pipe';
+import { DashIfNothingPipe } from './pipes/dash-if-nothing.pipe';
+import { JoinWithAndPipe } from './pipes/join-with-and.pipe';
+import { LargestPipe } from './pipes/largest.pipe';
+import { MonthToNamePipe } from './pipes/month-to-name.pipe';
+import { NameValuePairPipe } from './pipes/name-value-pair.pipe';
 import { NumberArrayPipe } from './pipes/number-array.pipe';
+import { NumberToDatePipe } from './pipes/number-to-date.pipe';
+import { RepeatTypeToNamePipe } from './pipes/repeat-type-to-name.pipe';
+import { StatusNamePipe } from './pipes/status-name.pipe';
+import { SumPipe } from './pipes/sum.pipe';
+import { TimeInIntervalPipe } from './pipes/time-in-interval.pipe';
+import { WeekdayToNamePipe } from './pipes/weekday-to-name.pipe';
 // eslint-disable-next-line import/order
 import { TranslateLoader, TranslateModule, TranslatePipe } from '@ngx-translate/core';
 // eslint-disable-next-line import/order
 import { TranslateHttpLoader } from '@ngx-translate/http-loader';
-import { IsDataPipe } from './pipes/is-data.pipe';
-import { TimeSlotsComponent } from './components/time-slots/time-slots.component';
-import { MatSpinnerComponent } from './components/mat-spinner/mat-spinner.component';
-import { RoleNamePipe } from './pipes/role-name.pipe';
-import { IsPermittedDirective } from './directives/permission.directive';
-import { UserRolePipe } from './pipes/user-role.pipe';
-import { UtcToLocalPipe } from './pipes/utc-to-local.pipe';
-import { DefaultDatePipe } from './pipes/default-date.pipe';
-import { ShowSlotPercentagePipe } from './pipes/showSlotPercentage.pipe';
 import { CompleteProfileComponent } from './components/complete-profile/complete-profile.component';
 import { LoginFailedComponent } from './components/login-failed/login-failed.component';
-
-import { DfmCalendarPickerComponent } from './components/dfm-calendar/dfm-calendar-picker/dfm-calendar-picker.component';
+import { MatSpinnerComponent } from './components/mat-spinner/mat-spinner.component';
+import { TimeSlotsComponent } from './components/time-slots/time-slots.component';
+import { IsPermittedDirective } from './directives/permission.directive';
+import { DefaultDatePipe } from './pipes/default-date.pipe';
+import { IsDataPipe } from './pipes/is-data.pipe';
+import { RoleNamePipe } from './pipes/role-name.pipe';
+import { ShowSlotPercentagePipe } from './pipes/showSlotPercentage.pipe';
+import { UserRolePipe } from './pipes/user-role.pipe';
+import { UtcToLocalPipe } from './pipes/utc-to-local.pipe';
 import { DateAdapter, MatNativeDateModule, MAT_DATE_LOCALE } from '@angular/material/core';
 import { MatDatepickerModule } from '@angular/material/datepicker';
+import { MatMenuModule } from '@angular/material/menu';
+import { DfmCalendarPickerComponent } from './components/dfm-calendar/dfm-calendar-picker/dfm-calendar-picker.component';
+import { MultiDropdownPlaceholderNamePipe } from './pipes/multi-dropdown-placeholder-name.pipe';
+import { DUTCH_BE } from './utils/const';
 import { CustomDatePickerAdapter } from './utils/date-adapter';
-import { DUTCH_BE } from "./utils/const";
-import {MultiDropdownPlaceholderNamePipe} from "./pipes/multi-dropdown-placeholder-name.pipe";
+
 export function HttpLoaderFactory(http: HttpClient) {
 	return new TranslateHttpLoader(http);
 }
@@ -112,7 +113,7 @@ export function HttpLoaderFactory(http: HttpClient) {
 		CompleteProfileComponent,
 		LoginFailedComponent,
 		DfmCalendarPickerComponent,
-		MultiDropdownPlaceholderNamePipe
+		MultiDropdownPlaceholderNamePipe,
 	],
 	imports: [
 		CommonModule,
@@ -139,6 +140,7 @@ export function HttpLoaderFactory(http: HttpClient) {
 		MatIconModule,
 		MatNativeDateModule,
 		MatDatepickerModule,
+		MatMenuModule,
 	],
 	exports: [
 		DesignSystemModule,
@@ -193,7 +195,8 @@ export function HttpLoaderFactory(http: HttpClient) {
 		MatIconModule,
 		DfmCalendarPickerComponent,
 		MatDatepickerModule,
-		MultiDropdownPlaceholderNamePipe
+		MultiDropdownPlaceholderNamePipe,
+		MatMenuModule,
 	],
 	providers: [
 		TranslatePipe,
