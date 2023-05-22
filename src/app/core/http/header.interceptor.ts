ac838dc1bcb5bfcf7173d491d90714d0236d83c7
ankit.kumar <ankit.kumar@mindrops.com>
2023-05-22 13:18:22 +0530

    admin - add session expired message

--- src/app/core/http/header.interceptor.ts
+++ src/app/core/http/header.interceptor.ts
@@ -1,13 +1,16 @@
-import {Injectable} from '@angular/core';
-import {HttpEvent, HttpHandler, HttpInterceptor, HttpRequest} from '@angular/common/http';
-import {Observable} from 'rxjs';
-import {environment} from "../../../environments/environment";
-import {UserService} from "../services/user.service";
-import {GeneralUtils} from "../../shared/utils/general.utils";
+import { HttpEvent, HttpHandler, HttpInterceptor, HttpRequest } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { EMPTY, Observable } from 'rxjs';
+import { Translate } from 'src/app/shared/models/translate.model';
+import { DUTCH_BE, ENG_BE } from 'src/app/shared/utils/const';
+import { environment } from '../../../environments/environment';
+import { GeneralUtils } from '../../shared/utils/general.utils';
+import { NotificationDataService } from '../services/notification-data.service';
+import { UserService } from '../services/user.service';
 
 @Injectable()
 export class HeaderInterceptor implements HttpInterceptor {
-	constructor(private userSvc: UserService) {}
+	constructor(private userSvc: UserService, public notificationSvc: NotificationDataService) {}
 
 	public intercept(request: HttpRequest<unknown>, next: HttpHandler): Observable<HttpEvent<unknown>> {
 		const SubDomain: string = window.location.host.split('.')[0];
@@ -15,15 +18,17 @@ export class HeaderInterceptor implements HttpInterceptor {
 
 		const sessionExp = localStorage.getItem('sessionExp');
 
-		// debugger;
+		if (sessionExp && +sessionExp < new Date().getTime()) {
+			this.userSvc.logout();
+			const lang = localStorage.getItem('lang');
+			setTimeout(() => {
+				this.notificationSvc.showError(Translate.sessionExp[lang!]);
+			}, 1500);
 
-		if (sessionExp) {
-			if (+sessionExp < new Date().getTime()) {
-				this.userSvc.logout();
-			} else {
-				GeneralUtils.saveSessionExpTime();
-			}
+			return EMPTY;
 		}
+
+		GeneralUtils.saveSessionExpTime();
 
 		// let currentTenantId = GeneralUtils.TenantID;
 		// const tenantIds = this.userSvc.getCurrentUser()?.tenantIds ?? [];
@@ -43,10 +48,4 @@ export class HeaderInterceptor implements HttpInterceptor {
 		return next.handle(newRequest);
 	}
 }
-
-
-
-
-
-
 
