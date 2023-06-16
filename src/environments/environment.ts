1500ed09013cf7b4e7352a66364497d78dffe759
Ankit Kumar <ankit.kumar@mindrops.com>
2023-06-16 10:53:19 +0530

    redirect uri changed for login

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -16,7 +16,7 @@ export const environment: Environment = {
 	schedulerApiUrl: 'https://diflexmo-scheduler-api-dev.azurewebsites.net/api',
 	userManagementApiUrl: 'https://auth.diflexmo.be/usermanagement/api',
 	authClientId: 'd526e147-4713-4a0a-bf56-d8f500fb9a62',
-	redirectUrl: window.location.origin,
+	redirectUrl: window.location.origin + '/admin',
 	schedulerApiAuthScope: 'https://diflexmoauth.onmicrosoft.com/cheduler.api/cheduler.api',
 };
 
