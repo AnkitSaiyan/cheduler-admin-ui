89fafe1948e0c8c5655db5d8a556aa49f36fc09b
Ankit Kumar <ankit.kumar@mindrops.com>
2023-07-07 15:24:17 +0530

    admin - week view calendar fix max limit time

--- src/app/shared/components/dfm-calendar/dfm-calendar-day-view/dfm-calendar-day-view.component.ts
+++ src/app/shared/components/dfm-calendar/dfm-calendar-day-view/dfm-calendar-day-view.component.ts
@@ -111,7 +111,6 @@ export class DfmCalendarDayViewComponent extends DestroyableComponent implements
 
 		const currentValue = changes['dataGroupedByDateAndRoom']?.currentValue;
 		const previousValue = changes['dataGroupedByDateAndRoom']?.previousValue;
-
 		if (JSON.stringify(currentValue) !== JSON.stringify(previousValue)) {
 			this.dataGroupedByDateAndRoom = currentValue;
 		}
