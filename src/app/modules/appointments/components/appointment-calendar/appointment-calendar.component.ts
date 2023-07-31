439a4a948aef5c79f3655aaa8141e946a8c2a7d9
Ankit Kumar <ankit.kumar@mindrops.com>
2023-07-31 17:12:58 +0530

    fix: pick up time in selected area

--- src/app/modules/appointments/components/appointment-calendar/appointment-calendar.component.ts
+++ src/app/modules/appointments/components/appointment-calendar/appointment-calendar.component.ts
@@ -339,7 +339,7 @@ export class AppointmentCalendarComponent extends DestroyableComponent implement
 						const currSD = new Date(appointment.startedAt);
 						const currED = new Date(appointment.endedAt);
 
-						if (currSD > startDate && currSD < endDate) {
+						if (currSD >= startDate && currSD <= endDate) {
 							sameGroup = true;
 							if (currED > endDate) {
 								endDate = currED;
