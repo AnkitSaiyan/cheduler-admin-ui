cb48ba50093174357120ee296d6cc601032e68b1
Ankit Kumar <ankit.kumar@mindrops.com>
2023-08-01 15:36:17 +0530

    testing slot bug 3

--- src/app/modules/appointments/components/add-appointment-modal/add-appointment-modal.component.ts
+++ src/app/modules/appointments/components/add-appointment-modal/add-appointment-modal.component.ts
@@ -291,7 +291,6 @@ export class AddAppointmentModalComponent extends DestroyableComponent implement
 	public handleSlotSelectionToggle(slots: SlotModified) {
 
 		AppointmentUtils.ToggleSlotSelection(slots, this.selectedTimeSlot, this.isCombinable);
-    debugger;
 		let smallestStartTime = '';
 		Object.values(this.selectedTimeSlot).forEach((slot) => {
 			if (!smallestStartTime || DateTimeUtils.TimeToNumber(slot.start) < DateTimeUtils.TimeToNumber(smallestStartTime)) {
