fa388d9985334c3b14b3a182756fa6c6c7aa387c
Ankit Kumar <ankit.kumar@mindrops.com>
2023-08-02 18:16:04 +0530

    fix: selected slot css

--- src/app/shared/pipes/find-selected-slot.pipe.ts
+++ src/app/shared/pipes/find-selected-slot.pipe.ts
@@ -2,10 +2,12 @@ import { Pipe, PipeTransform } from '@angular/core';
 
 @Pipe({
 	name: 'findSelectedSlot',
+	pure: false,
 })
 export class FindSelectedSlotPipe implements PipeTransform {
 	transform(selectedSlot: any, value: any[]): any {
 		return selectedSlot?.[value?.find((item) => selectedSlot[item])];
 	}
 }
+
 
