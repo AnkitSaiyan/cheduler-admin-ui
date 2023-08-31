360cce6d1452f26d9c06401377999bde0bca721a
Ankit Kumar <ankit.kumar@mindrops.com>
2023-08-31 16:13:51 +0530

    repeat form UI wip

---
+++ src/app/shared/components/repeat-form/repeat-form.component.ts
@@ -0,0 +1,13 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+	selector: 'dfm-repeat-form',
+	templateUrl: './repeat-form.component.html',
+	styleUrls: ['./repeat-form.component.scss'],
+})
+export class RepeatFormComponent implements OnInit {
+	constructor() {}
+
+	ngOnInit() {}
+}
+
--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -80,6 +80,8 @@ import { SafePipe } from './pipes/safe.pipe';
 import { FindSelectedSlotPipe } from './pipes/find-selected-slot.pipe';
 import { RemoveDuplicateDataPipe } from './pipes/remove-duplicate-data.pipe';
 import { DfmAutoScrollInViewDirective } from './directives/dfm-auto-scroll-in-view.directive';
+import { RepeatFormComponent } from './components/repeat-form/repeat-form.component';
+import { MatTabsModule } from '@angular/material/tabs';
 
 export function HttpLoaderFactory(http: HttpClient) {
 	return new TranslateHttpLoader(http);
@@ -135,6 +137,7 @@ export function HttpLoaderFactory(http: HttpClient) {
 		SafePipe,
 		FindSelectedSlotPipe,
 		RemoveDuplicateDataPipe,
+		RepeatFormComponent,
 	],
 	imports: [
 		CommonModule,
@@ -164,6 +167,7 @@ export function HttpLoaderFactory(http: HttpClient) {
 		MatDatepickerModule,
 		MatMenuModule,
 		MatSlideToggleModule,
+		MatTabsModule,
 	],
 	exports: [
 		DesignSystemModule,
@@ -228,6 +232,8 @@ export function HttpLoaderFactory(http: HttpClient) {
 		MatMenuModule,
 		InfiniteScrollModule,
 		MatSlideToggleModule,
+		RepeatFormComponent,
+		MatTabsModule,
 	],
 	providers: [
 		TranslatePipe,
