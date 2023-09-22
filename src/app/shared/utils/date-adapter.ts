b4beab938d044be2596682d0eded1bd3a809ec4b
sonu-mindrops <sonu@mindrops.com>
2023-09-22 15:25:12 +0530

    fix date picker translation

--- src/app/shared/utils/date-adapter.ts
+++ src/app/shared/utils/date-adapter.ts
@@ -1,35 +1,29 @@
-import { Injectable } from '@angular/core';
-import { NativeDateAdapter } from '@angular/material/core';
-import { DUTCH_BE } from './const';
+// import { Injectable } from '@angular/core';
+// import { NativeDateAdapter } from '@angular/material/core';
+// import { DUTCH_BE } from './const';
 
 
 
 
-@Injectable({ providedIn: 'root' })
-export class CustomDatePickerAdapter extends NativeDateAdapter {
-	override getDayOfWeekNames() {
-		const language = localStorage.getItem('lang');
-		if (language === DUTCH_BE) {
-			return ['Zo', 'Ma', 'Di', 'Wo', 'Do', 'Vr', 'Za'];
-		}
-		return ['Su', 'Mo', 'Tu', 'We', 'Th', 'Fr', 'Sa'];
-	}
+// @Injectable({ providedIn: 'root' })
+// export class CustomDatePickerAdapter extends NativeDateAdapter {
+// 	override getDayOfWeekNames() {
+// 		const language = localStorage.getItem('lang');
+// 		if (language === DUTCH_BE) {
+// 			return ['Zo', 'Ma', 'Di', 'Wo', 'Do', 'Vr', 'Za'];
+// 		}
+// 		return ['Su', 'Mo', 'Tu', 'We', 'Th', 'Fr', 'Sa'];
+// 	}
 
-	override getFirstDayOfWeek(): number {
-		return 1;
-	}
+// 	override getFirstDayOfWeek(): number {
+// 		return 1;
+// 	}
 
-	override getMonthNames(): string[] {
-		const language = localStorage.getItem('lang');
-		if (language === DUTCH_BE) {
-			return ['Jan', 'Feb', 'Mar', 'Apr', 'Mei', 'Jun', 'Jul', 'Aug', 'Sep', 'Okt', 'Nov', 'Dec'];
-		}
-		return ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'];
-	}
-}
-
-
-
-
-
-
+// 	override getMonthNames(): string[] {
+// 		const language = localStorage.getItem('lang');
+// 		if (language === DUTCH_BE) {
+// 			return ['Jan', 'Feb', 'Mar', 'Apr', 'Mei', 'Jun', 'Jul', 'Aug', 'Sep', 'Okt', 'Nov', 'Dec'];
+// 		}
+// 		return ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'];
+// 	}
+// }
