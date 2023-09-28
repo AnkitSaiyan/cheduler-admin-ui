e22bc92d4e098b4ef70943c1efdc2170c51fc58f
Ankit Kumar <ankit.kumar@mindrops.com>
2023-09-28 12:46:41 +0530

    fix: add exam validator error and fix placeholder of body part and body type

--- src/app/modules/exam/components/add-exam/add-exam.component.ts
+++ src/app/modules/exam/components/add-exam/add-exam.component.ts
@@ -363,7 +363,7 @@ export class AddExamComponent extends DestroyableComponent implements OnInit, On
 		let valid = true;
 
 		if (!this.examForm.valid) {
-			['name', 'expensive', 'roomType'].forEach((value) => {
+			['name', 'expensive', 'roomType', 'bodyPart', 'bodyType'].forEach((value) => {
 				this.examForm.controls[value].markAsTouched();
 			});
 			(this.examForm.controls['roomsForExam'] as FormArray).controls.forEach((control) => {
