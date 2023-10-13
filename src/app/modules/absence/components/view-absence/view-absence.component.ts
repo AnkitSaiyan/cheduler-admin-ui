452e65dade1e762ae303060a25dd3d084b3173da
sonu-mindrops <sonu@mindrops.com>
2023-10-13 12:23:39 +0530

    add static response and rander table

--- src/app/modules/absence/components/view-absence/view-absence.component.ts
+++ src/app/modules/absence/components/view-absence/view-absence.component.ts
@@ -1,18 +1,18 @@
 import { Component, OnDestroy, OnInit } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
-import { BehaviorSubject, filter, map, switchMap, take, takeUntil, tap } from 'rxjs';
+import { BehaviorSubject, filter, map, switchMap, take, takeUntil } from 'rxjs';
 import { ShareDataService } from 'src/app/core/services/share-data.service';
 import { Permission } from 'src/app/shared/models/permission.model';
 import { AbsenceApiService } from '../../../../core/services/absence-api.service';
 import { ModalService } from '../../../../core/services/modal.service';
 import { NotificationDataService } from '../../../../core/services/notification-data.service';
-import { RouterStateService } from '../../../../core/services/router-state.service';
 import { ConfirmActionModalComponent, ConfirmActionModalData } from '../../../../shared/components/confirm-action-modal.component';
 import { DestroyableComponent } from '../../../../shared/components/destroyable.component';
 import { Absence, RepeatType } from '../../../../shared/models/absence.model';
 import { Translate } from '../../../../shared/models/translate.model';
 import { ABSENCE_ID, ABSENCE_TYPE, ABSENCE_TYPE_ARRAY, ENG_BE } from '../../../../shared/utils/const';
 import { AddAbsenceComponent } from '../add-absence/add-absence.component';
+import { DfmDatasource, DfmTableHeader } from 'diflexmo-angular-design';
 
 @Component({
 	selector: 'dfm-view-absence',
@@ -28,15 +28,27 @@ export class ViewAbsenceComponent extends DestroyableComponent implements OnInit
 
 	public readonly Permission = Permission;
 
-	public columns = ['AppointmentNo', 'Exam', 'StartDate', 'EndDate', 'PatientName', 'Edit'];
+	public columns = ['AppointmentNo', 'PatientName', 'StartDate', 'EndDate'];
 
 	public absenceType$$ = new BehaviorSubject<(typeof ABSENCE_TYPE_ARRAY)[number]>(ABSENCE_TYPE_ARRAY[0]);
 
 	effectedAppointments$$: BehaviorSubject<any> = new BehaviorSubject<any[]>([]);
 
+	public tableData$$ = new BehaviorSubject<DfmDatasource<any>>({
+		items: [],
+		isInitialLoading: true,
+		isLoadingMore: false,
+	});
+
+	public tableHeaders: DfmTableHeader[] = [
+		{ id: '1', title: 'AppointmentNo', isSortable: true },
+		{ id: '2', title: 'PatientName', isSortable: true },
+		{ id: '3', title: 'StartDate', isSortable: true },
+		{ id: '4', title: 'EndDate', isSortable: true },
+	];
+
 	constructor(
 		private absenceApiSvc: AbsenceApiService,
-		private routerStateSvc: RouterStateService,
 		private notificationSvc: NotificationDataService,
 		private router: Router,
 		private modalSvc: ModalService,
@@ -47,7 +59,8 @@ export class ViewAbsenceComponent extends DestroyableComponent implements OnInit
 	}
 
 	public ngOnInit(): void {
-    this.route.data
+		this.getAppointmentListModified();
+		this.route.data
 			.pipe(
 				filter((data) => !!data[ABSENCE_TYPE]),
 				map((data) => data[ABSENCE_TYPE]),
@@ -70,7 +83,24 @@ export class ViewAbsenceComponent extends DestroyableComponent implements OnInit
 		this.shareDataService
 			.getLanguage$()
 			.pipe(takeUntil(this.destroy$$))
-			.subscribe((lang) => (this.selectedLang = lang));
+			.subscribe((lang) => {
+				this.selectedLang = lang;
+				this.tableHeaders = this.tableHeaders.map((h, i) => ({
+					...h,
+					title: Translate[this.columns[i]][lang],
+				}));
+			});
+
+		this.effectedAppointments$$.pipe(takeUntil(this.destroy$$)).subscribe({
+			next: (value) => {
+				this.tableData$$.next({
+					items: value,
+					isInitialLoading: false,
+					isLoading: false,
+					isLoadingMore: false,
+				});
+			},
+		});
 	}
 
 	public deleteAbsence(id: number) {
@@ -115,5 +145,92 @@ export class ViewAbsenceComponent extends DestroyableComponent implements OnInit
 				keyboard: false,
 			},
 		});
+	}
+
+	private getAppointmentListModified() {
+		const impactedAppointmentDetails = [
+			{
+				appointmentId: 263,
+				patientFname: 'niti',
+				patientLname: 'yadav',
+				examDetails: [
+					{
+						examId: 39,
+						roomId: 0,
+						startedAt: '2024-02-10 11:50:00',
+						endedAt: '2024-02-10 12:00:00',
+					},
+				],
+			},
+			{
+				appointmentId: 265,
+				patientFname: 'niti',
+				patientLname: 'yadav',
+				examDetails: [
+					{
+						examId: 39,
+						roomId: 0,
+						startedAt: '2024-02-10 10:30:00',
+						endedAt: '2024-02-10 10:45:00',
+					},
+					{
+						examId: 40,
+						roomId: 0,
+						startedAt: '2024-02-10 10:40:00',
+						endedAt: '2024-02-10 10:55:00',
+					},
+				],
+			},
+			{
+				appointmentId: 264,
+				patientFname: 'niti',
+				patientLname: 'yadav',
+				examDetails: [
+					{
+						examId: 39,
+						roomId: 0,
+						startedAt: '2024-02-10 10:30:00',
+						endedAt: '2024-02-10 10:40:00',
+					},
+					{
+						examId: 40,
+						roomId: 0,
+						startedAt: '2024-02-10 10:40:00',
+						endedAt: '2024-02-10 10:55:00',
+					},
+				],
+			},
+		];
+		this.effectedAppointments$$.next(impactedAppointmentDetails.map((appointment) => this.getAppointmentModified(appointment)));
+	}
+
+	private getAppointmentModified(appointment) {
+		let startedAt;
+		let endedAt;
+
+		let { patientFname, patientLname, ...rest } = appointment;
+
+		appointment.examDetails.forEach((exam) => {
+			if (exam.startedAt && (!startedAt || new Date(exam.startedAt) < startedAt)) {
+				startedAt = new Date(exam.startedAt);
+			}
+
+			if (exam.endedAt && (!endedAt || new Date(exam.endedAt) > endedAt)) {
+				endedAt = new Date(exam.endedAt);
+			}
+		});
+		return {
+			...rest,
+			patientFullName: `${patientFname} ${patientLname}`,
+			startedAt: startedAt,
+			endedAt: endedAt,
+		};
+	}
+
+	public navigateToView(e: any) {
+		if (e?.appointmentId) {
+			// localStorage.setItem('previousPagefromView', 'dashboard');
+			this.router.navigate([`/appointment/${e.appointmentId}/view`], { replaceUrl: true });
+		}
 	}
 }
