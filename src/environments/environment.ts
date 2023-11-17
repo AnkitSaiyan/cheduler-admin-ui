188548762bd86fa23bfbcaa68c628b6fc6f1d5c1
Ankit Kumar <ankit.kumar@mindrops.com>
2023-11-17 16:59:10 +0530

    fix: "userManagementApiUrl" changed

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -14,7 +14,7 @@ interface Environment {
 export const environment: Environment = {
 	production: false,
 	schedulerApiUrl: 'https://diflexmo-scheduler-api-dev.azurewebsites.net/api',
-	userManagementApiUrl: 'https://auth.diflexmo.be/usermanagement/api',
+	userManagementApiUrl: 'https://auth-dev.diflexmo.be/usermanagement/api',
 	authClientId: 'cf1fa2ba-8272-4875-b6c2-0cea52ee30a2',
 	redirectUrl: window.location.origin + '/admin',
 	schedulerApiAuthScope: 'https://diflexmoauthdev.onmicrosoft.com/cheduler.api/cheduler.api',
